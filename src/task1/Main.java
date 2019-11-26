b62e5f5a8cdf7892f8329a7f45420b7c921c0a80
smalex <alexdate@gmail.com>
2019-11-26 20:08:27 +0300

    First commit

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -1,22 +1,21 @@
 package task1;
 
-import org.w3c.dom.ls.LSOutput;
-
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Enumeration;
 
 public class Main {
-    
-    public static void main(String[] args) {
 
+    public static ArrayList<Character> s = new ArrayList<>();
+    public static void main(String[] args) {
         try(InputStream in = new FileInputStream("test.txt")){
             int x = 0;
+            while((x = in.read()) != -1){
+                s.add((char)x);
+            }
 
-            while ((x = in.read()) != -1) {
-                System.out.println((byte) (x));
+            int i = 0;
+            for (int j = 0; j < s.size(); j++){
+                System.out.print(s.get(j));
             }
         }catch (IOException ex){
             System.out.println(ex.getMessage());
