431c898d0d757fdac7f5846e224eb52f62e606c7
smilex <alexdate@gmail.com>
2019-11-28 00:00:03 +0300

    Second commit

--- src/task2/Main.java
+++ src/task2/Main.java
@@ -1,16 +1,18 @@
 package task2;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.ArrayList;
-
+import java.util.LinkedList;
+//TODO почему-то пишется 3 раза
 
 public class Main {
 
-    public static ArrayList<Character> s = new ArrayList<>();
+    public static LinkedList<Character> s = new LinkedList<>();
+    private static String strong;
 
     public static void readFromFile(String fileName){
+        strong = "";
+
         try(InputStream in = new FileInputStream(fileName)){
             int x = 0;
             while((x = in.read()) != -1){
@@ -19,6 +21,7 @@ public class Main {
 
             int i = 0;
             for (int j = 0; j < s.size(); j++){
+                writeIntoFile("all.txt", s.get(j));
                 System.out.print(s.get(j));
             }
         }catch (IOException ex){
@@ -26,14 +29,28 @@ public class Main {
         }
     }
 
-    public static void writeIntoFile(String fileName){
-        try()
+    public static void writeIntoFile(String fileNameToRecord, Character s){
+        try(OutputStream out = new FileOutputStream(fileNameToRecord, true)) {
+            int i = 0;
+            out.write(s);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
         readFromFile("test.txt");
         readFromFile("test2.txt");
         readFromFile("test3.txt");
+        readFromFile("test4.txt");
+        readFromFile("test5.txt");
+/*        writeIntoFile("all.txt", "test.txt");
+        writeIntoFile("all.txt", "test2.txt");
+        writeIntoFile("all.txt", "test3.txt");
+        writeIntoFile("all.txt", "test4.txt");
+        writeIntoFile("all.txt", "test5.txt");*/
     }
 
 }
