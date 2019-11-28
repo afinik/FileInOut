d47df43f65604ef7a17ce51d69f335df501fb3cc
smilex <alexdate@gmail.com>
2019-11-28 21:29:15 +0300

    Second commit

--- src/task2/Main.java
+++ src/task2/Main.java
@@ -1,27 +1,32 @@
 package task2;
 
+import javax.naming.CompoundName;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.LinkedList;
-//TODO почему-то пишется 3 раза
 
 public class Main {
 
-    public static LinkedList<Character> s = new LinkedList<>();
     private static String strong;
+    private static Boolean weNeverWriteToAllFile = true;
+
+    private static String nameOfAllFile = "all";
 
     public static void readFromFile(String fileName){
         strong = "";
+        LinkedList<Character> s = new LinkedList<>();
 
         try(InputStream in = new FileInputStream(fileName)){
             int x = 0;
+
             while((x = in.read()) != -1){
                 s.add((char)x);
             }
 
             int i = 0;
+
             for (int j = 0; j < s.size(); j++){
-                writeIntoFile("all.txt", s.get(j));
+                writeIntoFile(nameOfAllFile + ".txt", s.get(j));
                 System.out.print(s.get(j));
             }
         }catch (IOException ex){
@@ -41,6 +46,10 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        while (new File(nameOfAllFile + ".txt").exists()){
+            nameOfAllFile = nameOfAllFile + "1";
+            System.out.println("The file " + nameOfAllFile + ".txt is exist");
+        }
         readFromFile("test.txt");
         readFromFile("test2.txt");
         readFromFile("test3.txt");
